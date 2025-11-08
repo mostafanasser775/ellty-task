16fe711812ebcec6d3b0bb25dff802c0395f9b7c
mostafanasser776 <dev3@falconsegypt.com>
2025-11-09 01:34:15 +0200

    added the env to the new db using postregess

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   },
   engine: "classic",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: env("POSTGRES_PRISMA_URL"),
+    directUrl: env("POSTGRES_URL_NON_POOLING"),
   },
 });
